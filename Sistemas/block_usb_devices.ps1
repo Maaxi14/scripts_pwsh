b3f4d9f502fbce8adb96b8e6dd979385e7dcb456
Máximo J. Moreno <mjx21@outlook.es>
2023-08-14 20:33:37 +0200

    Added parameters to script

--- Sistemas/block_usb_devices.ps1
+++ Sistemas/block_usb_devices.ps1
@@ -1,29 +1,33 @@
-#Computer\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\StorageDevicePolicies
+param(
+    [Parameter(Mandatory=$True, 
+    HelpMessage="Ruta de la clave de registro a buscar - EX: 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\RemovableStorageDevices'")]
+    [string]$Key_path
+)
 
 ## Crear una nueva clave -- RemovableStorageDevices DWORD 32-bit
 # HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows
 
 # Comprobamos si existe la clave
-$ruta_clave = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\RemovableStorageDevices"
-$clave = "RemovableStorageDevices"
+# $ruta_clave = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\RemovableStorageDevices"
+# $clave = "RemovableStorageDevices"
 
 try {
     
-    if ((Get-ChildItem -Path $ruta_clave -ErrorAction SilentlyContinue) -eq "") {
+    if ((Get-ChildItem -Path $Key_path -ErrorAction SilentlyContinue) -eq "") {
         # Clave existe - seteamos a 1
         Write-Host "Clave existe - seteamos la clave a 1"
-        Set-ItemProperty -Path $ruta_clave -Name "Deny_All" -Value "1" -Verbose
+        Set-ItemProperty -Path $Key_path -Name "Deny_All" -Value "1" -Verbose
     } else {
         #Creamos clave
-        New-Item -Path $ruta_clave -Verbose -ErrorAction SilentlyContinue
+        New-Item -Path $Key_path -Verbose -ErrorAction SilentlyContinue
         Write-Host -ForegroundColor Yellow "Clave ya existe, paramos la ejecución"
         Exit 0
-        New-ItemProperty -Path $ruta_clave -Name "Deny_All" -Value "1" -PropertyType DWORD -Verbose -ErrorAction Stop
+        New-ItemProperty -Path $Key_path -Name "Deny_All" -Value "1" -PropertyType DWORD -Verbose -ErrorAction Stop
     }
 
 } catch {
         #Creamos clave
-        New-Item -Path $ruta_clave -Verbose
-        New-ItemProperty -Path $ruta_clave -Name "Deny_All" -Value "1" -PropertyType DWORD -Verbose
+        New-Item -Path $Key_path -Verbose
+        New-ItemProperty -Path $Key_path -Name "Deny_All" -Value "1" -PropertyType DWORD -Verbose
 
 } 
